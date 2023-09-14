5f44c5f6bfde08a22b10c552117f237fb713cd96
Arvind kumar <arvindkr40882@gmail.com>
2023-09-14 11:45:36 +0530

    Static Files & Installing Pug template engine

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,21 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
 const port =80;
+
+// for serving static file 
+app.use('/static',express.static('static'))
+
+// set the template engine as pug 
+app.set('view engine', 'pug')
+
+// set the views directory 
+app.set('views',path.join(__dirname,'views'))
+
+// our pug demo end point
+app.get('/demo', (req, res) => {
+    res.render('demo', { title: 'Hey', message: 'Thanks for telling me how to use pug engine ' })
+  })
 
 app.get("/",(req,res)=>{
  res.send("this is my first express app with Arvind")
---
+++ Express/static/index.js
@@ -0,0 +1,1 @@
+console.log("Arvind is a good boys")
